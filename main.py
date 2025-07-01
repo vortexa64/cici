b8a1451338bcee508d6cf4bd78308bf20d00d092
vortexa64 <aryagantengsepuh@gmail.com>
2025-07-01 21:20:11 +0700

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-import time
-import requests
-
-url = "https://youtube.com/shorts/y1miCbXwHBg?si=Kkp1d2d7IjSzmuJ0"  # ganti linkmu di sini
-
-while True:
-    print("Nonton video...")
-    requests.get(url)
-    time.sleep(60)
